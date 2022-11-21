c0680db66b87a446ef25320594d81fafe508514d
Suchitra Natarajan <sucnat@gmail.com>
2022-11-21 10:16:33 -0500

    .gitignnore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/fides/overview":["static\u002Fchunks\u002Fpages\u002Ffides\u002Foverview.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ffides\u002Foverview"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
