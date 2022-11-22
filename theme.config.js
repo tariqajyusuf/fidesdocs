7f82d0f6c8658babc17620a1425ff843bb95a73a
Suchitra Natarajan <sucnat@gmail.com>
2022-11-22 15:22:17 -0500

    slack link

--- theme.config.js
+++ theme.config.js
@@ -149,6 +149,8 @@ export default {
           </div>
           <div className="socials">
             <div className="icons"> 
+
+                <a href="https://www.fid.es/join-slack" rel="noopener"> <img src ="/assets/slack.svg" /> </a>
                 <a href="https://www.linkedin.com/company/ethyca/" rel="noopener"> <img src ="/assets/linkedin.svg" /> </a>
                 <a href="https://twitter.com/ethyca" rel="noopener">  <img src ="/assets/twitter.svg" /> </a>
                 <a href="https://github.com/ethyca/fides" rel="noopener">  <img src ="/assets/github.svg" /> </a>
