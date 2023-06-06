88743c1bd39544e7bd6a4be8ce8486abca547676
Cillian Kieran <cilliankieran@Cillian-Office-MBP.local>
2023-06-06 16:29:57 -0400

    Image URL updates

--- theme.config.js
+++ theme.config.js
@@ -149,16 +149,16 @@ export default {
             rel="noopener"
             className="inline-flex items-center no-underline text-current font-semibold"
           >
-            <img src ="/fides/assets/ethyca_logo.svg" />
+            <img src ="/assets/ethyca_logo.svg" />
           </a>
           </div>
           <div className="socials">
             <div className="icons"> 
 
-                <a href="https://www.fid.es/join-slack" rel="noopener"> <img src ="/fides/assets/slack.svg" /> </a>
-                <a href="https://www.linkedin.com/company/ethyca/" rel="noopener"> <img src ="/fides/assets/linkedin.svg" /> </a>
-                <a href="https://twitter.com/ethyca" rel="noopener">  <img src ="/fides/assets/twitter.svg" /> </a>
-                <a href="https://github.com/ethyca/fides" rel="noopener">  <img src ="/fides/assets/github.svg" /> </a>
+                <a href="https://www.fid.es/join-slack" rel="noopener"> <img src ="/assets/slack.svg" /> </a>
+                <a href="https://www.linkedin.com/company/ethyca/" rel="noopener"> <img src ="/assets/linkedin.svg" /> </a>
+                <a href="https://twitter.com/ethyca" rel="noopener">  <img src ="/assets/twitter.svg" /> </a>
+                <a href="https://github.com/ethyca/fides" rel="noopener">  <img src ="/assets/github.svg" /> </a>
             </div>
               <HubspotForm
                    portalId='7252764'
