6639c3be838dcafb75e249fab210de5e297dbf45
Cillian Kieran <cilliankieran@Cillian-Office-MBP.local>
2023-06-06 16:07:40 -0400

    Testing path resolution for images

--- next.config.js
+++ next.config.js
@@ -7,23 +7,6 @@ const withNextra = require("nextra")({
   defaultShowCopyCode: true,
 });
 
-module.exports = withNextra({
-  basePath: '/fides',  
-  redirects: () => {
-    return [
-      {
-        source: "/fides",
-        destination: "/",
-        statusCode: 301,
-      },
-      {
-        source: '/fides/dsr_support/:slug*',
-        destination: '/fides/dsr_quickstart/dsr_support/:slug*', // Matched parameters can be used in the destination
-        permanent: true,
-      }
-    ];
-  }
-
-});
+module.exports = withNextra();
 
 
