e08ff71b740487890060e4541a1dbe21aeeee45e
Suchi Natarajan <suchitra@ethyca.com>
2023-06-08 15:59:36 -0400

    community docs and cpra to ccpa edits"

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
-  unstable_flexsearch: false,
+  flexsearch: false,
   unstable_staticImage: true,
   defaultShowCopyCode: true,
 });
