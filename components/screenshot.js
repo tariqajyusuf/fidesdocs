ca9a0cee91da9e7d980f13fc473ce16ab08fc7b9
Cillian Kieran <1268052+cilliankieran@users.noreply.github.com>
2023-06-18 22:34:28 -0400

    Complete data mapping tutorial

--- components/screenshot.js
+++ components/screenshot.js
@@ -1,6 +1,5 @@
 
 export default function Screenshot({ img, description, mw }) {
-console.log('mw : '+mw);
   return (
     <span className="screenshot">
      <img alt={description} src={img} width={mw}/>
