5147724fdcd03b2e15b47fb190c50d9469ff5a2f
Suchitra Natarajan <suchitra@ethyca.com>
2023-07-18 12:01:06 -0400

    Add gtm (#148)

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { SkipNavLink } from '@reach/skip-nav'
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head> 
+            <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PZ6FM4T6');
+      `}
+      </Script>
+        </Head> 
         <body>
                <noscript
           dangerouslySetInnerHTML={{
