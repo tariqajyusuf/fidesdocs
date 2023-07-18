705cbe508dcc7b5fd92e1494dc8caec01de7f87d
Suchitra Natarajan <suchitra@ethyca.com>
2023-07-18 11:28:15 -0400

    Add gtm to head and body (#145)

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+               <noscript
+          dangerouslySetInnerHTML={{
+            __html: '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZ6FM4T6"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
+          }}
+      />
           <SkipNavLink />
           <Main />
           <NextScript />
