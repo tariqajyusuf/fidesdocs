f6e619327e78ef6f9f8d71629f6fcd94847a58a3
Cillian <1268052+cilliankieran@users.noreply.github.com>
2023-09-21 11:04:58 -0400

    Fixed hydration error related to malformed HTML injection for Vimeo  (#226)

--- components/vimeovideo.js
+++ components/vimeovideo.js
@@ -1,11 +1,12 @@
 
 export default function VimeoVideo({ src }) {
 
-  var html = "   <div dangerouslySetInnerHTML={{ __html: '<div style='padding:56.25% 0 0 0;position:relative;margin:10px 0px;'><iframe src=" + src + " frameborder='0' allow='autoplay; fullscreen; picture-in-picture' allowfullscreen style='position:absolute;top:0;left:0;width:100%;height:100%;' title='Privacy Orientation'></iframe></div><script src='https://player.vimeo.com/api/player.js'></script></div>";
   return (
-    <>
-       <div dangerouslySetInnerHTML={{ __html:html}} />
- 
-    </>
+    <div style={{position: 'relative'}}>
+    <div style={{ padding:'56.25% 0 0 0', margin: 'relative', margin:' 10px 0px' }}>
+        <iframe src={src} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style={{ position: 'absolute' , top: '0', left:'0', width: '100%', height: '100%' }} title="Privacy Orientation"></iframe>
+    </div>
+    <script src="https://player.vimeo.com/api/player.js"></script>
+    </div>
   );
 }
