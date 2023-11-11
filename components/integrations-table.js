f15be135d9987310d56dc2ce6ec1bb3f98c1a71a
Adrian Galvan <adrian@ethyca.com>
2023-11-10 16:08:50 -0800

    Starting point for integrations table component

---
+++ components/integrations-table.js
@@ -0,0 +1,18 @@
+export default function IntegrationsTable({ columns }) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <a
+              href="/user-guides/integrations/saas-integrations/adobe-campaign"
+              className="nx-text-primary-600 nx-underline"
+            >
+              <strong>Adobe Campaign</strong>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
